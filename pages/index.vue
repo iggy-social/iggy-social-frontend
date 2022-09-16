3e5fffb53f1090bc4c1109bcfbca5b320e766e1c
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-09-16 17:13:44 +0200

    index options api

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,52 @@
-<script setup>
-import { MetaMaskConnector, WalletConnectConnector, CoinbaseWalletConnector, useBoard, useEthers } from 'vue-dapp'
-
-const { open } = useBoard()
-const { address } = useEthers()
-
-const infuraId = ''
-
-const connectors = [
-  new MetaMaskConnector({
-    appUrl: 'http://localhost:3000',
-  }),
-  new WalletConnectConnector({
-    qrcode: true,
-    rpc: {
-      1: `https://mainnet.infura.io/v3/${infuraId}`,
-      4: `https://rinkeby.infura.io/v3/${infuraId}`,
-    },
-  }),
-  new CoinbaseWalletConnector({
-    appName: 'Vue Dapp',
-    jsonRpcUrl: `https://mainnet.infura.io/v3/${infuraId}`,
-  }),
-]
-</script>
-
 <template>
   <div class="container mt-4">
+    <h1>Connect your wallet:</h1>
+
     <button class="btn btn-primary" @click="open">Connect Wallet</button>
+
+    <button class="btn btn-primary mx-3" @click="smthElse">Smth else</button>
+
     <vd-board :connectors="connectors" dark />
     {{ address }}
   </div>
 </template>
+
+<script>
+import { MetaMaskConnector, WalletConnectConnector, CoinbaseWalletConnector, useBoard, useEthers } from 'vue-dapp'
+
+export default {
+  name: "index",
+
+  methods: {
+    smthElse() {
+      console.log("smth else")
+    }
+  },
+
+  setup() {
+    const { open } = useBoard()
+    const { address } = useEthers()
+
+    const infuraId = ''
+
+    const connectors = [
+      new MetaMaskConnector({
+        appUrl: 'http://localhost:3000',
+      }),
+      new WalletConnectConnector({
+        qrcode: true,
+        rpc: {
+          1: `https://mainnet.infura.io/v3/${infuraId}`,
+          4: `https://rinkeby.infura.io/v3/${infuraId}`,
+        },
+      }),
+      new CoinbaseWalletConnector({
+        appName: 'Vue Dapp',
+        jsonRpcUrl: `https://mainnet.infura.io/v3/${infuraId}`,
+      }),
+    ]
+
+    return { address, connectors, open }
+  }
+}
+</script>
