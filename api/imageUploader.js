d36ff47b0f6ea1ff2dd70d879f0f84bcd0d9db40
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-12-07 09:47:13 +0100

    Update imageUploader.js

--- api/imageUploader.js
+++ api/imageUploader.js
@@ -5,7 +5,7 @@ export default async function handler(request, response) {
     const bucketName = process.env.SPHERON_BUCKET_NAME; // enter bucket name in environment variables
     const token = process.env.SPHERON_STORAGE_TOKEN; // add spheron storage token in environment variables
     
-    const protocol = ProtocolEnum.IPFS;
+    const protocol = ProtocolEnum.IPFS; // choose IPFS or Arweave
  
     const client = new SpheronClient({ token });
  
