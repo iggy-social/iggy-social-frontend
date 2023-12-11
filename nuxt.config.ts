6568a419be0a77b2716a875e6134b6b4727ab923
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-12-11 15:12:31 +0100

    SVG cover images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -99,15 +99,15 @@ export default defineNuxtConfig({
       orbisContext: "kjzl6cwe1jw14b86b0dwz8x7ribwcn9tsull1bphner0nyfdwxsqnohbtrrvgtl", // production context
       orbisTest: false, // if true, test context will be used instead of the production one
       orbisTestContext: "kjzl6cwe1jw145tfqv2eqv8tiz6puo27meyz4smz40atppuc13tulqca87k35z2", // test context
-      previewImage: "/img/cover.png",
-      previewImageAirdrop: "/img/cover-airdrop.png",
-      previewImageNftCollection: "/img/cover-nft-collection.png",
-      previewImageNftCreate: "/img/cover-nft-create.png",
-      previewImageNftLaunchpad: "/img/cover-nft-launchpad.png",
-      previewImagePost: "/img/cover-post.png",
-      previewImagePostNft: "/img/cover-post-nft.png",
-      previewImageProfile: "/img/cover-profile.png",
-      previewImageStake: "/img/cover-stake.png",
+      previewImage: "/img/covers/cover.svg",
+      previewImageAirdrop: "/img/covers/cover-airdrop.svg",
+      previewImageNftCollection: "/img/covers/cover-nft-collection.svg",
+      previewImageNftCreate: "/img/covers/cover-nft-create.svg",
+      previewImageNftLaunchpad: "/img/covers/cover-nft-launchpad.svg",
+      previewImagePost: "/img/covers/cover-post.svg",
+      previewImagePostNft: "/img/covers/cover-post-nft.svg",
+      previewImageProfile: "/img/covers/cover-profile.svg",
+      previewImageStake: "/img/covers/cover-stake.svg",
       profileMintedPostIdsMax: 36, // max number of minted post ids to show in the profile page
       projectMetadataTitle: "Iggy Social Demo | Web3 Social Template For Your DAO",
       projectName: "Iggy Demo",
