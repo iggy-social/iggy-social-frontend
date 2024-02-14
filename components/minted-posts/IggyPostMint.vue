22cadf2bfd1b323874d409bf72d8fd34c9e394e7
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-02-14 19:28:21 +0100

    watch chain id

--- components/minted-posts/IggyPostMint.vue
+++ components/minted-posts/IggyPostMint.vue
@@ -118,7 +118,6 @@ export default {
       if (sanitizedText.length > 183) {
         //this.textPreview = sanitizedText.replace(/[^\x00-\x7F]/g, "").substring(0, 180) + "..."; // uncomment if you want to remove non-ascii characters (e.g. emojis)
         this.textPreview = sanitizedText.substring(0, 180) + "...";
-        console.log(this.textPreview);
       } else if (sanitizedText.length === 0) {
         this.textPreview = "";
       } else {
@@ -276,6 +275,15 @@ export default {
     return {
       address, chainId, isActivated, signer, toast, userStore
     }
+  },
+
+  watch: {
+    chainId() {
+      if (this.isSupportedChain) {
+        this.fetchMintData();
+      }
+    }
   }
+
 }
 </script>
