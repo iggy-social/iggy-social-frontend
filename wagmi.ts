c248fd1e3bdce0e38c53b85c64122958fad02bca
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2025-09-11 18:07:18 +0200

    custom chain added

--- wagmi.ts
+++ wagmi.ts
@@ -1,10 +1,44 @@
 import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector'
 import { http, cookieStorage, createConfig, createStorage } from '@wagmi/vue'
-import { arbitrumSepolia } from '@wagmi/vue/chains'
 import { injected, metaMask, walletConnect } from '@wagmi/vue/connectors'
+import type { Chain } from 'viem'
+
+export const customArbitrumSepolia: Chain = {
+  id: 421_614,
+  name: 'Arbitrum Sepolia',
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: [
+        'https://sepolia-rollup.arbitrum.io/rpc',
+        'https://arbitrum-sepolia.drpc.org',
+        'https://arbitrum-sepolia.therpc.io',
+        'https://arbitrum-sepolia.gateway.tenderly.co',
+      ],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Arbiscan',
+      url: 'https://sepolia.arbiscan.io',
+      apiUrl: 'https://api-sepolia.arbiscan.io/api',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 81930,
+    },
+  },
+  testnet: true,
+}
 
 export const config = createConfig({
-  chains: [arbitrumSepolia],
+  chains: [customArbitrumSepolia],
   connectors: [
     injected(),
     metaMask(),
@@ -18,7 +52,7 @@ export const config = createConfig({
   }),
   ssr: true,
   transports: {
-    [arbitrumSepolia.id]: http(),
+    [customArbitrumSepolia.id]: http(),
   },
 })
 
