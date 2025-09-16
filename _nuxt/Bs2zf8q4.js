5c2fb6c6e7ea12a6809258042d9a7a9eca71ce52
tempe-techie <95053628+tempe-techie@users.noreply.github.com>
2025-09-16 21:59:27 +0000

    Deploying to build from @ iggy-social/iggy-social-frontend@3b92e8c1b80733a0a1d5b6caab3045bde7a92061 🚀

---
+++ _nuxt/Bs2zf8q4.js
@@ -0,0 +1,8 @@
+import{m as o}from"./BN2Zqmnd.js";import"#entry";import"./3vkYfJSL.js";const r=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M0 3a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1Zm2.63 5.25a1 1 0 0 1 1-1h8.75a1 1 0 1 1 0 2H3.63a1 1 0 0 1-1-1Zm2.62 5.25a1 1 0 0 1 1-1h3.5a1 1 0 0 1 0 2h-3.5a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{r as filtersSvg};
